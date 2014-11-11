3e8bb6670eb9b50998e91b5d5f27de9878cf2ab9
Kyle Windsor <kwindsor@uwo.ca>
2014-11-11 16:52:24 -0500

    Added basic inventory and adding items (to be modified)

--- gamemode/sh_items.lua
+++ gamemode/sh_items.lua
@@ -1,4 +1,6 @@
-GM.Items = { };
+local meta = FindMetaTable( "Player" );
+
+GM.MetaItems = { };
 
 local files = file.Find( GM.FolderName .. "/gamemode/items/*.lua", "LUA", "namedesc" );
 
@@ -28,8 +30,124 @@ if( #files > 0 ) then
 		
 		ITEM.Class = string.StripExtension( v );
 		
-		GM.Items[string.StripExtension( v )] = ITEM;
+		GM.MetaItems[string.StripExtension( v )] = ITEM;
 		
 	end
+	
+end
+
+function GM:GetMetaItem( class )
+	
+	return self.MetaItems[class];
+	
+end
+
+function GM:Item( class )
+	
+	local tab = { };
+	tab.Class = class;
+	tab.Vars = self:GetMetaItem( class ).Vars; -- Initialize default variables
+	
+	return tab;
+	
+end
+
+function meta:CheckInventory()
+	
+	if( !self.Inventory ) then
+		
+		self.Inventory = { };
+		
+	end
+	
+end
+
+function meta:IsInventorySlotOccupied( x, y )
+	
+	self:CheckInventory();
+	
+	for _, v in pairs( self.Inventory ) do
+		
+		if( x > v.X and x <= v.X + GAMEMODE:GetMetaItem( v.Class ).W ) then
+			
+			if( y > v.Y and y <= v.Y + GAMEMODE:GetMetaItem( v.Class ).H ) then
+				
+				return true;
+				
+			end
+			
+		end
+		
+	end
+	
+	return false;
+	
+end
+
+function meta:GetNextAvailableSlot( w, h )
+	
+	self:CheckInventory();
+	
+	local x = 1;
+	local y = 1;
+	
+	for i = 1, 6 do
+		
+		for j = 1, 10 do
+			
+			if( !self:IsInventorySlotOccupied( i, j ) ) then
+				
+				local good = true;
+				
+				if( x + w <= 6 or y + h <= 10 ) then
+					
+					for x = 1, w do
+						
+						for y = 1, h do
+							
+							if( self:IsInventorySlotOccupied( x, y ) ) then
+								
+								good = false;
+								
+							end
+							
+						end
+						
+					end
+					
+					if( good ) then
+						
+						return i, j;
+						
+					end
+					
+				end
+				
+			end
+			
+		end
+		
+	end
+	
+	return -1, -1;
+	
+end
+
+function meta:GiveItem( item )
+	
+	self:CheckInventory();
+	
+	if( type( item ) == "string" ) then
+		
+		item = GAMEMODE:Item( item );
+		
+	end
+	
+	local x, y = self:GetNextAvailableSlot( GAMEMODE:GetMetaItem( item.Class ).W, GAMEMODE:GetMetaItem( item.Class ).H );
+	
+	item.X = x;
+	item.Y = y;
+	
+	table.insert( self.Inventory, item );
 	
 end
